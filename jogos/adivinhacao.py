1fbc326153bf11148227499a3fd4723f68468387
henriqfaria <henriqfaria@gmail.com>
2023-11-23 11:51:51 -0300

    commit com as atualizações do curso

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -25,27 +25,27 @@ def jogar():
     for rodada in range(1, total_de_tentativas + 1):
         print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-        chute_str = input("Digite um número entre 1 e 100: ")
-        print("Você digitou " , chute_str)
-        chute = int(chute_str)
+        tentativa_str = input("Digite um número entre 1 e 100: ")
+        print("Você digitou " , tentativa_str)
+        tentativa = int(tentativa_str)
 
-        if(chute < 1 or chute > 100):
+        if(tentativa < 1 or tentativa > 100):
             print("Você deve digitar um número entre 1 e 100!")
             continue
 
-        acertou = chute == numero_secreto
-        maior   = chute > numero_secreto
-        menor   = chute < numero_secreto
+        acertou = tentativa == numero_secreto
+        maior   = tentativa > numero_secreto
+        menor   = tentativa < numero_secreto
 
         if(acertou):
             print("Você acertou e fez {} pontos!".format(pontos))
             break
         else:
             if(maior):
-                print("Você errou! O seu chute foi maior do que o número secreto.")
+                print("Você errou! O seu tentativa foi maior do que o número secreto.")
             elif(menor):
-                print("Você errou! O seu chute foi menor do que o número secreto.")
-            pontos_perdidos = abs(numero_secreto - chute)
+                print("Você errou! O seu tentativa foi menor do que o número secreto.")
+            pontos_perdidos = abs(numero_secreto - tentativa)
             pontos = pontos - pontos_perdidos
 
     print("Fim do jogo")
--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,35 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "bergamota".lower()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    # enquanto não enforcou e não acertou
+    while ((not enforcou) and (not acertou)):
+
+        tentativa = input("Qual letra? ")
+        tentativa = tentativa.strip().lower()
+
+        if (tentativa in palavra_secreta):
+            for index, letra in enumerate(palavra_secreta):
+                if (tentativa == letra):
+                    letras_acertadas[index] = letra
+        else:
+            erros += 1
+        enforcou = (erros == 6)
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if (acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
